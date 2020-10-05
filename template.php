6a50bebda8e8223b9e129989c955d936f7a8177b
arseny_kadilov <arsenykadilov@gmail.com>
2020-10-05 18:36:00 +0300

    solution

--- template.php
+++ template.php
@@ -13,8 +13,8 @@
         <button>Get result</button>
     </form>
 
-    <?php if ($questionAnswered): ?>
-        <?php if ($answerPositive): ?>
+    <?php if ($questionAnswered === TRUE): ?>
+        <?php if ($answerPositive === TRUE): ?>
             <img src="https://gamebanana.com/sprays/embeddables/77252?type=sdt_image" alt="Yes, honey">
         <?php else: ?>
             <img src="https://i.pinimg.com/736x/4a/27/77/4a2777f259f837a53ca818dedc27766e.jpg" alt="Noes">
